bf15081e705212f250f18c3a24666ab2b4b8063d
DOSWIN <ayomidedosunmuoreoluwa@gmail.com>
2023-03-10 23:17:20 +0100

    Hero Section revamp

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,25 +3,25 @@ import { curve, heroSectionImage, videoPlayIcon } from '../assets'
 
 const HeroSection = () => {
   return (
-    <div className='lg:py-[40px] py-6 flex items-center bg-[#e5d4ff2d] lg:px-[100px] px-10 gap-10'>
+    <div className='lg:py-[40px] py-6 flex lg:flex-row flex-col  items-center bg-[#e5d4ff2d] lg:px-[100px] px-10 gap-10'>
       <div>
         <span className="font-medium text-[15px] text-[#A2A2A2]">
           2021 BEST FREELANCE SOFTWARE AWARDS
         </span>
-        <h1 className="relative font-bold text-[50px] text-black">
+        <h1 className="relative font-bold text-[32px] lg:text-[50px] text-black mt-2 w-[85%]">
           Hire The <span className='text-primary'>Best </span>
           Freelancers Around The  World
 
-          <img src={curve} alt="" className='absolute -bottom-4 left-[44%]' />
+          <img src={curve} alt="" className='absolute -bottom-6 left-[44%]' />
         </h1>
-        <p className="font-normal text-[18px] text-dimBlack my-10">
+        <p className="font-normal text-[16px] lg:text-[18px] text-dimBlack my-10 lg:w-[90%] w-[86%]">
           World’s Leading Freelance Platform, Get the best freelancers and work with top major companies.
         </p>
         <div className="flex gap-4">
-          <button className="btn1">
+          <button className="btn1 text-[16px] lg:text-[18px]">
             Hire a freelancer
           </button>
-          <button className="btn2 flex gap-2 items-center">
+          <button className="btn2 flex gap-2 items-center text-[16px] lg:text-[18px]">
             <img src={videoPlayIcon} alt="" />
             See how it works
           </button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { navLinks } from '../constants'
 
 const Navbar = () => {
   return (
-    <div className='px-4 md:px-10 lg:px-[100px] py-[20px] lg:py-6 flex items-center justify-between text-black bg-[#FBF9FF] border-b border-[] fixed left-0 right-0'>
+    <div className='px-4 md:px-10 lg:px-[100px] py-[20px] lg:py-6 flex items-center justify-between text-black bg-[#FBF9FF] border-b border-[] fixed left-0 right-0 z-50'>
       <Link to='/'>
-        <img src={gighutLogo} alt="" />
+        <img src={gighutLogo} alt="" className='h-5 w-[180px]' />
       </Link>
-      <div className="lg:flex hidden items-center gap-6">
+      <div className="lg:flex hidden items-center justify-end w-full gap-10">
         {
           navLinks.map(navLink => (
             <Link to='' className='hover:text-gray-500'>
@@ -18,8 +18,8 @@ const Navbar = () => {
           ))
         }
       </div>
-      <div className="hidden items-center gap-6 lg:flex">
-        <div className="relative border px-12 h-[40px] flex items-center rounded-full">
+      <div className="hidden items-center gap-6 lg:flex justify-end">
+        <div className="relative border px-12 h-[40px] flex items-center rounded-full w-[40%]">
           <input type="text" className="outline-none bg-transparent" placeholder='Search' />
           <img src={searchIcon} alt="" className="absolute top-2 left-4" />
         </div>
